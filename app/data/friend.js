4bddb6dcb019a69ebfe141df3927a0555889dbc7
Tasha <natalytak@yahoo.com>
2019-04-13 23:05:21 -0400

    initial load

---
+++ app/data/friend.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+    {
+      name: "Sarah",
+      imageLink: "www.sarah.com",
+      answer1: "1",
+      answer2: "1",
+      answer3: "2",
+      answer4: "2",
+      answer5: "3",
+      answer6: "3",
+      answer7: "4",
+      answer8: "4",
+      answer9: "5",
+      answer10: "5",
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
